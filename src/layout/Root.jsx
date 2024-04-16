be9e3a65ffebc484d83d040398303170601cd1fd
nhwnahid007 <nhwnahid@gmail.com>
2024-04-16 15:06:23 +0600

    added favicon and navbar section

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/navbar/Navbar";
 
 
 const Root = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
---
+++ src/shared/navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <FaBars className="text-2xl" />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost flex items-center text-xl"> <img className="w-7" src="https://i.ibb.co/mJ1wHr2/primeplaces.png" alt="" /> Prime Places</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
